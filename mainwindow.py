0801f1e6fc692f6ab4340b16d6f8f1ba0833ea39
davidescobar17 <davidescobar17@outlook.com>
2022-05-14 16:19:37 +0930

    Add directory browsing and selection

--- mainwindow.py
+++ mainwindow.py
@@ -2,8 +2,7 @@ import sys
 from PyQt5.QtCore import QDir
 from PyQt5.QtWidgets import (QMainWindow, QWidget, QTreeView, QFileSystemModel,
     QGridLayout, QApplication, QLabel, QComboBox, QPushButton, QSizePolicy,
-    QGridLayout, QHBoxLayout)
-from PyQt5.QtGui import QKeySequence
+    QGridLayout, QHBoxLayout, QFileDialog)
 
 
 class MainWindow(QMainWindow):
@@ -32,18 +31,43 @@ class MainWindow(QMainWindow):
                 self.filterComboBox = QComboBox(self)
                 self.filterComboBox.setEditable(True)
                 self.filterComboBox.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
-                Layout.addWidget(self.filterComboBox, 0, 1)
+                Layout.addWidget(self.filterComboBox, 0, 1, 1, 2)
 
-                Layout.addWidget(self.treeView, 1, 0, 1, 2)
+                directoryLabel = QLabel("In directory:")
+                Layout.addWidget(directoryLabel, 1, 0)
+                self.directoryComboBox = QComboBox(self)
+                self.directoryComboBox.setEditable(True)
+                self.directoryComboBox.addItem(QDir.currentPath())
+                self.directoryComboBox.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+                Layout.addWidget(self.directoryComboBox, 1, 1)
+                self.browseButton = QPushButton('Browse', self)
+                self.browseButton.clicked.connect(self.browse)
+                Layout.addWidget(self.browseButton, 1, 2)
+
+                Layout.addWidget(self.treeView, 2, 0, 1, 3)
 
                 buttonsLayout = QHBoxLayout()
                 buttonsLayout.addStretch()
                 self.findButton = QPushButton('Find', self)
                 self.findButton.clicked.connect(self.find)
                 buttonsLayout.addWidget(self.findButton)
-                Layout.addLayout(buttonsLayout, 2, 0, 1, 2)
+                Layout.addLayout(buttonsLayout, 3, 0, 1, 3)
 
                 widget.setLayout(Layout)
+
+        def browse(self):
+            directory = QFileDialog.getExistingDirectory(self, "Find Files",
+                QDir.currentPath())
+
+            if directory:
+                if self.directoryComboBox.findText(directory) == -1:
+                    self.directoryComboBox.addItem(directory)
+
+                self.directoryComboBox.setCurrentIndex(self.directoryComboBox.findText(directory))
+
+                root = self.fileSystemModel.setRootPath(directory)
+                self.treeView.setModel(self.fileSystemModel)
+                self.treeView.setRootIndex(root)
 
         def find(self):
             self.fileSystemModel.setNameFilters(["*" + self.filterComboBox.currentText() + "*"])
